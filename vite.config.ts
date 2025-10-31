aaab8cf10e6de11e7b0b771c4de4a1294dc31e85
siyasjr <siyasjr@gmail.com>
2025-10-31 21:42:06 +0530

    vite config misc

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,23 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
+   server: {
+    proxy: {
+      '/api/sheets': {
+        target: 'https://script.google.com/macros/s/AKfycbwe2Wkbw_ZTxqzlpyk1PpMB8laceMbLpbJYHMSVR9lGFMOstS_FjaK4I6rdqFTwyCI/exec',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api\/sheets/, ''),
+      },
+    },
   },
+
+
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+
+  
 }));
